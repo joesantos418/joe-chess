c132ec6a4d014f1fcfe4bac5894004099b2e6c14
joesantos418 <joesantos386@gmail.com>
2023-08-23 19:08:28 -0300

    skhelethons

--- pkg/rules/generate_moves.go
+++ pkg/rules/generate_moves.go
@@ -7,7 +7,7 @@ func generateMoves(c Color, b board.Board) []Move {
 	for _, n := range board.SquareNames {
 		p := b.GetPiece(n)
 		if isColor(p, c) {
-			moves = append(moves, generateMovesForPiece(p, b))
+			moves = append(moves, generateMovesForPiece(p, n, b)...)
 		}
 	}
 
@@ -18,34 +18,79 @@ func isColor(p board.Piece, c Color) bool {
 	return false
 }
 
-func generateMovesForPiece(p board.Piece, b board.Board) []Move {
+// we gotta consider more than one of the same piece on the board
+func generateMovesForPiece(p board.Piece, n board.SquareName, b board.Board) []Move {
 	var moves []Move
 
 	if movesVertically(p) {
-		moves = append(moves, generateVericalMoves(p, b))
+		moves = append(moves, generateVericalMoves(p, n, b)...)
 	}
 
 	if movesHorizontally(p) {
-		moves = append(moves, generateHorizontalMoves(p, b))
+		moves = append(moves, generateHorizontalMoves(p, n, b)...)
 	}
 
 	if movesDiagonally(p) {
-		moves = append(moves, generateDiagonalMoves(p, b))
+		moves = append(moves, generateDiagonalMoves(p, n, b)...)
 	}
 
 	if movesInL(p) {
-		moves = append(moves, generateLMoves(p, b))
+		moves = append(moves, generateLMoves(p, n, b)...)
 	}
 
 	if isPawn(p) {
 		// en passant as well
-		moves = append(moves, generatePawnCaptureMoves(p, b))
+		moves = append(moves, generatePawnCaptureMoves(p, n, b)...)
 	}
 
 	if isKing(p) {
 		// castle as well
-		moves = append(moves, generateCastleMoves(p, b))
+		moves = append(moves, generateCastleMoves(p, n, b)...)
 	}
 
 	return moves
+}
+
+func movesVertically(p board.Piece) bool {
+	return false
+}
+
+func movesHorizontally(p board.Piece) bool {
+	return false
+}
+
+func movesDiagonally(p board.Piece) bool {
+	return false
+}
+
+func movesInL(p board.Piece) bool {
+	return false
+}
+
+func isPawn(p board.Piece) bool {
+	return false
+}
+
+func generateVericalMoves(p board.Piece, n board.SquareName, b board.Board) []Move {
+	return []Move{}
+}
+
+func generateHorizontalMoves(p board.Piece, n board.SquareName, b board.Board) []Move {
+	return []Move{}
+}
+
+func generateDiagonalMoves(p board.Piece, n board.SquareName, b board.Board) []Move {
+	return []Move{}
+}
+
+func generateLMoves(p board.Piece, n board.SquareName, b board.Board) []Move {
+	return []Move{}
+}
+
+func generatePawnCaptureMoves(p board.Piece, n board.SquareName, b board.Board) []Move {
+	return []Move{}
+}
+
+func generateCastleMoves(p board.Piece, n board.SquareName, b board.Board) []Move {
+	return []Move{}
 }
